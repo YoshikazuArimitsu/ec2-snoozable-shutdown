7a52d24d1ab71457328ad0f71d804ed3d158d25c
Y.Arimitsu <yoshikazu_arimitsu@albert2005.co.jp>
2021-08-19 16:26:47 +0900

    開発時間3時間 ドキュメント15分

---
+++ README.md
@@ -0,0 +1,87 @@
+# 有光式スヌーズ機能付き EC2 インスタンス停止 Lambda
+
+## これは何をするものか
+
+![image](image.png)
+
+AWSの自動停止ソリューションは数あるもののどれも機能が貧弱なのと、
+よく発生する「止め忘れは困るけど今日は学習するから回しておきたい・・・」というニーズに応えられるものが無かった為、Azure VM の自動シャットダウンをパクって車輪を再発明したものです。
+
+## インストール方法
+
+1. 新規Lambdaを作成し、lambda_function.pyをコピペする
+   
+   * タイムアウト3秒はキツイので伸ばす
+   * Lambdaの実行ロールに EC2 の操作ポリシーアタッチしておくのを忘れない
+ 
+2. API Gateway を作る
+   
+   * API Gateway に POST メソッドを作り、作った Lambda を呼び出す設定を行う
+  
+3. Slack アプリを作ってインストール
+   
+   * Interactve Components を有効化し、Interactivity & Shortcuts のリクエストURLに API Gateway のメソッドのURLを入れる
+   * Botを有効化し、トークンを取っておく
+   * ワークスペース・通知チャンネルに追加する
+  
+4. 諸々の情報をソース先頭の情報にコピペする
+   
+   * AWSリージョン
+   * SlackのBOTトークン
+   * SlackのチャンネルID
+
+5. Cloud Watch Event で適当に起動間隔を設定する
+
+6. 操作対象EC2のタグ ec2-snoozable-shutdown に停止時刻と何分前にリマインドするか入れておく
+
+    ```
+    {"defaultShutdown": "1930", "remind": "10"}
+    ```
+
+(*) verbose_notification = True だと起動・停止検出時にもSlackにメッセージを送信します。邪魔なら外してください。
+
+
+## 開発仕様メモ
+
+### タグ
+
+　余計なDBは作りたくないのでEC2のタグに状態を入れて気合で管理する。
+
+```json
+{
+    "defaultShutdown": "1900",                     // 停止時刻
+    "remind" : "10",                               // リマインドメッセージ送るタイミング(予定時刻 n分前)
+    "state" : "running",                           // 前回のstate
+    "shutdownSchedule" : "YYYY-MM-DD hh:mm:ss",    // シャットダウン予定時刻
+    "sendRemind": True                             // 通知送信済か？
+}
+```
+
+### 挙動
+
+一定間隔で起動、ec2-snoozable-shutdown タグが入っているEC2インスタンスに対し定時処理。
+
+* running
+
+  * "shutdownSchedule" なし  
+    defaultShutdownからその日(もしくは次の日)の停止時刻を決め、shutdownScheduleに入れる
+
+  * "shutdownSchedule" あり
+    * 現在時刻 > shutdownSchedule  
+      EC2停止
+
+    * 現在時刻 > shutdownSchedule - remind分 & sendRemind = False  
+      リマインド送信  
+      sendRemind = True
+
+* stopped
+
+  * "state" = "running"  
+    shutdownSchedule削除  
+    sendRemind 削除  
+
+* アクションメッセージ受信時
+
+  shutdownSchedule更新  
+  sendRemind=False
+
---
+++ lambda_function.py
@@ -0,0 +1,277 @@
+import boto3
+import json, logging
+import traceback
+import urllib.request
+from datetime import datetime, timedelta, timezone
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+JST = timezone(timedelta(hours=+9), 'JST')
+
+# 
+verbose_notification = True
+aws_region = 'ap-northeast-1'
+slack_bot_token = 'xoxb-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+slack_channel = 'G01952TR7DK'
+schedule_tag = 'ec2_snoozable_shutdown'
+
+reminder_template = """
+[
+		{
+			"type": "section",
+			"text": {
+				"type": "plain_text",
+				"text": "%message-text%"
+			}
+		},
+		{
+			"type": "divider"
+		},
+		{
+			"type": "actions",
+			"elements": [
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": "1時間延長する",
+						"emoji": true
+					},
+					"style": "primary",
+					"value": "%machine%;%datetime-1%"
+				},
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": "3時間延長する",
+						"emoji": true
+					},
+					"style": "primary",
+					"value": "%machine%;%datetime-2%"
+				},
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": "今日はシャットダウンしない",
+						"emoji": true
+					},
+					"style": "danger",
+					"value": "%machine%;%datetime-3%"
+				}
+			]
+		}
+]
+"""
+
+def parse_tag(instance, key):
+    t = [t for t in instance.tags if t['Key'] == key]
+    if len(t) > 0:
+        return t[0]['Value']
+    return None
+
+def instance_desc(instance):
+    return f'{instance.id}({parse_tag(instance, "Name")})'
+
+def post_message(message):
+    url = "https://slack.com/api/chat.postMessage"
+    headers = {
+        "Content-Type": "application/json; charset=UTF-8",
+        "Authorization": f"Bearer {slack_bot_token}"
+    }
+
+    data = {
+        "channel": slack_channel,
+        "blocks": json.dumps(message),
+    }
+    logger.info(message)
+    
+    req = urllib.request.Request(url, json.dumps(data).encode(), headers)
+    with urllib.request.urlopen(req) as res:
+        body = res.read()
+        logger.info(body)
+
+def post_plain(message):
+    template = """[
+		{
+			"type": "section",
+			"text": {
+				"type": "plain_text",
+				"emoji": true,
+				"text": "%message%"
+			}
+		}
+    ]
+"""
+    m = template
+    m = m.replace("%message%", message)
+    message = json.loads(m)
+
+    post_message(message)
+
+def post_remind(instance, stopTime):
+    def stoptime(dt, ts):
+        d = dt + ts
+        return d.strftime('%Y-%m-%d_%H:%M:%S%z')
+
+    m = reminder_template
+    m = m.replace("%message-text%", f'{instance_desc(instance)} は {stopTime} に停止予定ですよ')
+    m = m.replace('%machine%', instance.id)
+    m = m.replace('%datetime-1%', stoptime(stopTime, timedelta(hours=1)))
+    m = m.replace('%datetime-2%', stoptime(stopTime, timedelta(hours=3)))
+    m = m.replace('%datetime-3%', stoptime(stopTime, timedelta(days=1)))
+    message = json.loads(m)
+
+    post_message(message)
+
+def process_running(instance, data):
+    if 'shutdownSchedule' not in data:
+        now = datetime.now(JST)
+        h = int(data['defaultShutdown'][:2])
+        m = int(data['defaultShutdown'][2:])
+        d = datetime(now.year, now.month, now.day, h, m, 0, tzinfo=JST)
+        if d < now:
+            # 既に停止予定時刻を過ぎていれば明日の同時刻
+            now = now + timedelta(days=1)
+            d = datetime(now.year, now.month, now.day, h, m, 0, tzinfo=JST)
+        logger.info(f'set shutdownSchedule {d}')
+        data['shutdownSchedule'] = d.strftime('%Y-%m-%d %H:%M:%S%z')
+    else:
+        now = datetime.now(JST)
+        stopTime = datetime.strptime(data['shutdownSchedule'], '%Y-%m-%d %H:%M:%S%z')
+        remindTime = stopTime - timedelta(minutes=int(data['remind']))
+        
+        if now > stopTime:
+            # シャットダウン実行
+            logger.info('invoke shutdown...')
+            instance.stop()
+
+        elif now > remindTime and 'sendRemind' not in data:
+            # リマインド送信
+            logger.info('send shutdown remind')
+            post_remind(instance, stopTime)
+            data['sendRemind'] = True
+        
+    if 'state' in data and data['state'] != "running":
+        logger.info('state -> running')
+        if verbose_notification:
+            post_plain(f'{instance_desc(instance)} が起動しました。\n自動停止予定時刻は {data["shutdownSchedule"]} です。')
+        
+
+    data['state'] = 'running'
+    return data
+
+    
+def process_stopped(instance, data):
+    if "state" in data and data['state'] != "stopped":
+        logger.info('state -> stopped')
+        if verbose_notification:
+            post_plain(f'{instance_desc(instance)} が停止しました。')
+        
+    if 'shutdownSchedule' in data:
+        del data['shutdownSchedule']
+        del data['sendRemind']
+
+    data['state'] = 'stopped'
+    return data
+    
+def ec2_poll():
+    ec2 = boto3.resource('ec2', aws_region)
+    instances = ec2.instances.filter(
+        Filters=[{'Name': 'instance-state-name', 'Values': ['pending','running','stopping','stopped']}])
+
+    for instance in instances:
+        # logger.info(instance.tags)
+        try:
+            logger.info(f"EC2 instance {instance.id}({parse_tag(instance, 'Name')})")
+            sdata = parse_tag(instance, schedule_tag)
+            if sdata is None:
+                continue
+            
+            data = json.loads(sdata)
+
+            newData = None        
+            stateName = instance.state['Name']
+            if stateName == 'running':
+                newData = process_running(instance, data)
+            if stateName == 'stopped':
+                newData = process_stopped(instance, data)
+            
+            if newData:
+                tags = [{
+                    "Key" : schedule_tag,
+                    "Value" : json.dumps(newData)
+                }]
+                instance.create_tags(Tags=tags)
+
+        except BaseException as e:
+            logger.error(f'{str(e)}')
+            print(traceback.format_exc())
+            
+    logger.info('ec2_poll completed.')
+
+def handle_action(action_value, response_url):
+    # 停止予定時刻の更新
+    ec2 = boto3.resource('ec2', aws_region)
+    instances = ec2.instances.filter(
+        Filters=[{'Name': 'instance-state-name', 'Values': ['pending','running','stopping','stopped']}])
+
+    values = action_value.split(';')
+    instance = [i for i in instances if i.id == values[0]][0]
+
+    stopTime = datetime.strptime(values[1], '%Y-%m-%d_%H:%M:%S%z')
+
+    sdata = parse_tag(instance, schedule_tag)
+    data = json.loads(sdata)
+
+    logger.info(f'update shutdownSchedule {stopTime}')
+
+    data['shutdownSchedule'] = stopTime.strftime('%Y-%m-%d %H:%M:%S%z')
+    del data['sendRemind']
+
+    tags = [{
+        "Key" : schedule_tag,
+        "Value" : json.dumps(data)
+    }]
+    instance.create_tags(Tags=tags)
+
+    # 元メッセージ削除
+    url = "https://slack.com/api/chat.postMessage"
+    headers = {
+        "Content-Type": "application/json; charset=UTF-8"
+    }
+
+    data = {
+        "delete_original": True,
+    }
+    
+    req = urllib.request.Request(response_url, json.dumps(data).encode(), headers)
+    with urllib.request.urlopen(req) as res:
+        body = res.read()
+        logger.info(body)
+
+    #
+    if verbose_notification:
+        post_plain(f'{instance_desc(instance)} の自動停止時刻を {stopTime} に延長しました。')
+
+
+def lambda_handler(event, context):
+    logger.info(event)
+
+    try:
+        body = urllib.parse.unquote(event['body'])
+        body = body.lstrip('payload=\n')
+        body = json.loads(body)
+        action_value = body['actions'][0]['value']
+        logger.info(f'accept action {action_value}')
+        handle_action(action_value, body['response_url'])
+
+    except BaseException as e:
+        ec2_poll()
+        
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
